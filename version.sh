3091165d91bf165b885040ebcba79d3dc0d8e200
白狐 <1522099983@qq.com>
2023-01-21 05:04:04 +0000

    update version.sh.
    Signed-off-by: 白狐 <1522099983@qq.com>

--- version.sh
+++ version.sh
@@ -12,7 +12,7 @@ echo $(($num%$max+$min))
 imeibh=$(baihuqq 100000000000000 999999999999999)
 sed -i '15s/.*/ \"imei\": \"wcnm\",/g' device-${qq}.json
 sed -i 15s/wcnm/${imeibh}/g device-${qq}.json
-echo -e "\033[34m 修复完成 [注:一次不行 多试几次 实在不行 换一个号] \033[0m";
+echo -e "\033[32m 修复完成 [注:一次不行 多试几次 实在不行 换一个号] \033[0m";
 else
 echo -e "\033[31m 错误: ${qq} 该账号没有登录过 \033[0m";
 fi
