08bd6a11e7f68b312b849d9bec9603c2d28182e1
白狐 <1522099983@qq.com>
2023-03-18 15:51:00 +0000

    更新
    Signed-off-by: 白狐 <1522099983@qq.com>

---
+++ YZ.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+sed -i 's/ports.ubuntu.com/mirrors.tuna.tsinghua.edu.cn/g' /etc/apt/sources.list
+apt update -y
+apt install curl wget git whiptail -y
+wget -O /usr/local/bin/bhyz https://gitee.com/baihu433/Ubuntu-Yunzai/raw/master/Yunzai-shell.sh >> wget.log 2>&1 &
+{
+for ((i = 0 ; i <= 100 ; i+=1))
+do
+    sleep 0.01s
+    echo $i
+done
+} | whiptail --gauge "未安装 正在安装" 6 60 0
+if ! [ -e "/usr/local/bin/bhyz" ];then
+whiptail --title "白狐≧▽≦" --msgbox \
+"安装失败 请检查网络" \
+8 25
+exit
+fi
+chmod +x /usr/local/bin/bhyz
+rm wget.log
+rm -rf .profile
+mv .profile.bak .profile
+until npm -v
+do
+bash <(curl -sL https://deb.nodesource.com/setup_18.x)
+apt remove nodejs -y
+apt update -y
+apt install -y nodejs
+done
+npm config set registry http://registry.npm.taobao.org/
+npm install -g npm
+npm install -g pnpm
+pnpm config set registry http://registry.npm.taobao.org/
+pnpm install -g pnpm
+if ! [ -x "$(command -v pnpm)" ];then
+    echo -e "\033[31m pnpm安装失败\033[0m";
+    exit
+fi
+apt-get install redis -y
+redis-server --daemonize yes
+apt-get install -y fonts-wqy-microhei fonts-wqy-zenhei
+pushd ~/
+git clone --depth=1 https://gitee.com/yoimiya-kokomi/Yunzai-Bot
+if ! [ -d ~/Yunzai-Bot ]
+then
+echo -e "\033[31m 项目克隆失败 请检查网络\033[0m";
+exit
+fi
+pushd ~/Yunzai-Bot
+pnpm install -P && pnpm install -P
+pushd $HOME
+git clone --depth=1 https://gitee.com/Ganyu256/chromium
+pushd chromium
+bash install.sh
+pushd $HOME
+rm -rf chromium
+rm -rf chromium
+pushd Yunzai-Bot
+pnpm install -P
