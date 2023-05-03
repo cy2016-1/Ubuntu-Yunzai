308e2979fb5f7dde4ab3a09c12f21755dcdd3838
baihu433 <1522099983@qq.com>
2023-05-03 10:54:08 +0800

    up

--- version.sh
+++ version.sh
@@ -29,10 +29,11 @@ if test -z "${icqq_latest}";then
     fi
 fi
 echo -e ${green} - ${cyan}最新版本为 ${blue}${icqq_latest}${background}
-sed -i 's/${icqq_local}/"icqq": "^${icqq_latest}",/g' package.json
-rm node_modules/icqq
+sed -i "s/${icqq_local}/\"icqq\": \"^${icqq_latest}\",/g" package.json
+pnpm uninstall icqq
 pnpm install icqq@latest -w
 pnpm install -P && pnpm install
+function device(){
 echo -e ${green}"#####"${cyan}白狐-Yunzai-Bot${green}"#####"${background}
 echo -e ${cyan}请选择您的登陆设备${background}
 echo ${green}"#########################"${background}
@@ -52,6 +53,42 @@ grep platform ${file}
 old_equipment=`grep platform ${file}`
 new_equipment="platform: 6"
 sed -i "s/${old_equipment}/${new_equipment}/g" $file
-rm data/device.json
+rm data/device.json > /dev/null
+rm data/icqq > /dev/null
+}
 echo
+echo -e ${green}"#####"${cyan}白狐-Yunzai-Bot${green}"#####"${background}
+echo -e ${cyan}请选择您的报错类型${background}
+echo ${green}"#########################"${background}
+echo -e ${green}1. ${cyan}错误码:${red}45${background}
+echo -e ${green}2. ${cyan}错误码:${red}235${background}
+echo -e ${green}3. ${cyan}错误码:${red}237${background}
+echo -e ${green}4. ${cyan}错误码:${red}238${background}
+echo -e ${green}5. ${cyan}仅更改登录端口(设备)${background}
+echo ${green}"#########################"${background}
+echo -e ${green}QQ群:狐狸窝:${cyan}705226976${background}
+echo -e ${green}注意:${cyan}手表协议和Macos协议都无法戳一戳,因为本身这两种设备都不支持.${background}
+echo ${green}"#########################"${background}
+echo -en ${green}请输入您的选项:${background} ;read number
+case number in
+1)
+echo -e ${cyan}错误码:${red}45'\n'${cayn}建议使用${yellow}MacOS或${yellow}iPad或${yellow}old_Android'\n'回车继续${background};read
+device
+;;
+2)
+echo -en ${cyan}错误码:${red}235'\n'${cayn}建议先使用手表协议然后扫码登录'\n'回车继续${background};read
+device
+;;
+3)
+pnpm uninstall icqq
+pnpm install icqq@0.2.3 -w
+echo -en ${cyan}错误码:${red}237'\n'${cayn}建议使用iPad协议登录'\n'回车继续${background};read
+device
+;;
+4)
+echo -en ${cyan}错误码:${red}238'\n'${cayn}建议命令换手表协议后再换回iPad协议[全部用密码]'\n'回车继续${background};read
+device
+;;
+esac
+pnpm run login
 echo ${green}执行完成 ${cyan}回车退出${background};read
