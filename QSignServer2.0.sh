e432e296d65f83f9abdd8143afcc688799205e96
root <root@localhost.localdomain>
2023-07-25 03:41:10 +0000

    up:更新签名服务器1.1.6

--- QSignServer2.0.sh
+++ QSignServer2.0.sh
@@ -15,6 +15,18 @@ if ! [ "$(uname)" = "Linux" ]; then
 	echo -e ${red}你是大聪明吗?${background}
     exit
 fi
+QSIGN_URL="https://ghproxy.com/https://github.com/fuqiuluo/unidbg-fetch-qsign/releases/download/1.1.6/unidbg-fetch-qsign-1.1.6.zip"
+QSIGN_VERSION="116"
+case $(uname -m) in
+amd64|x86_64)
+JDK_URL="https://cdn.azul.com/zulu/bin/zulu8.70.0.23-ca-jdk8.0.372-linux_x64.tar.gz"
+node=x64
+;;
+arm64|aarch64)
+JDK_URL="https://cdn.azul.com/zulu-embedded/bin/zulu8.70.0.23-ca-jdk8.0.372-linux_aarch64.tar.gz"
+node=arm64
+;;
+esac
 function main(){
 cd $HOME
 if [ -d $HOME/QSignServer/jdk ];then
@@ -51,16 +63,6 @@ if [ ! $(command -v git) ] || [ ! $(command -v axel) ] || [ ! $(command -v gzip)
         apk add git axel gzip unzip xz
     fi
 fi
-case $(uname -m) in
-amd64|x86_64)
-JDK_URL="https://cdn.azul.com/zulu/bin/zulu8.70.0.23-ca-jdk8.0.372-linux_x64.tar.gz"
-node=x64
-;;
-arm64|aarch64)
-JDK_URL="https://cdn.azul.com/zulu-embedded/bin/zulu8.70.0.23-ca-jdk8.0.372-linux_aarch64.tar.gz"
-node=arm64
-;;
-esac
 JAVA_VERSION=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}')
 if [[ ! "${JAVA_VERSION}" == "1.8"* ]]; then
     until axel -n 32 -o jdk.tar.gz -c ${JDK_URL}
@@ -123,7 +125,7 @@ if ! [ -x "$(command -v pnpm)" ];then
     echo
 fi
 if ! [ -x "$(command -v pm2)" ];then
-    echo -e ${yellow}正在使用pnpm安装pnpm${background}
+    echo -e ${yellow}正在使用pnpm安装pm2${background}
     pnpm config set registry https://registry.npmmirror.com
     pnpm config set registry https://registry.npmmirror.com
     until pnpm install -g pm2@latest
@@ -133,38 +135,13 @@ if ! [ -x "$(command -v pm2)" ];then
     done
     echo
 fi
-
-echo -e ${white}"====="${green}白狐-QSignServer${white}"====="${background}
-echo -e ${cyan}请选择您要安装的版本${background}
-echo -e  ${green}1.  ${cyan}1.1.5${background}
-echo -e  ${green}2.  ${cyan}1.1.0${background}
-echo "========================="
-echo -e ${cyan}他们的区别是${background}
-echo -e ${cyan}1.1.5适配icqq 1.1.0适配go-cqhttp${background}
-echo "========================="
-echo -en ${green}请输入您的选项: ${background};read number
-case ${number} in
-1|1.1.5)
-QSIGN_URL="https://ghproxy.com/https://github.com/fuqiuluo/unidbg-fetch-qsign/releases/download/1.1.5/qsign-1.1.5.onejar.zip"
-QSIGN_VERSION="115"
-;;
-2|1.1.0)
-QSIGN_URL="https://ghproxy.com/https://github.com/fuqiuluo/unidbg-fetch-qsign/releases/download/1.1.0/unidbg-fetch-qsign-1.1.0.zip"
-QSIGN_VERSION="110"
-;;
-*)
-echo
-echo -e ${red}输入错误${background}
-exit
-;;
-esac
 if [ -d $HOME/QSignServer/qsign${QSIGN_VERSION} ];then
 echo -e ${yellow}您已安装过该版本的签名服务器了${background}
 exit
 fi
 if [ ! -d $HOME/QSignServer/txlib ];then
 mkdir -p $HOME/QSignServer/txlib
-git clone --depth=1 -b 1.1.5 https://gitee.com/baihu433/unidbg-fetch-qsign
+git clone --depth=1 -b 1.1.6 https://gitee.com/baihu433/unidbg-fetch-qsign
 rm -rf $HOME/QSignServer/txlib
 mv -f unidbg-fetch-qsign/txlib $HOME/QSignServer/txlib
 rm -rf unidbg-fetch-qsign
@@ -184,17 +161,10 @@ echo -en ${yellow}安装完成 回车返回${background};read
 
 function start_QSignServer(){
 echo -e ${white}"====="${green}白狐-QSignServer${white}"====="${background}
-echo -e ${cyan}请选择您要启动的签名服务器${background}
-echo -e  ${green}1.  ${cyan}1.1.5${background}
-echo -e  ${green}2.  ${cyan}1.1.0${background}
-echo "========================="
-echo -en ${green}请输入您的选项: ${background};read number
-
-echo -e ${white}"====="${green}白狐-QSignServer${white}"====="${background}
 echo -e ${cyan}请选择您想让您签名服务器适配的QQ版本${background}
 echo -e  ${green}1.  ${cyan}8.9.63${background}
 echo -e  ${green}2.  ${cyan}8.9.68${background}
-#echo -e  ${green}2.  ${cyan}8.9.70${background}
+echo -e  ${green}3.  ${cyan}8.9.70${background}
 echo "========================="
 echo -en ${green}请输入您的选项: ${background};read num
 case ${num} in
@@ -204,25 +174,26 @@ export version=8.9.63
 2|8.9.68)
 export version=8.9.68
 ;;
+3|8.9.70)
+export version=8.9.70
+;;
 *)
 echo
 echo -e ${red}输入错误${background}
 exit
 ;;
 esac
-case ${number} in
-1|1.1.5)
-if [ ! -d $HOME/QSignServer/qsign115 ];then
+if [ ! -d $HOME/QSignServer/qsign116 ];then
 echo -e ${yellow}您未安装过该版本的签名服务器${background}
 exit
 fi
-if ! pm2 show qsign115 | grep -q online ;then
-    pm2 start --name qsign115 "bash $HOME/QSignServer/qsign115/bin/unidbg-fetch-qsign --basePath=$HOME/QSignServer/txlib/${version}"
+if ! pm2 show qsign116 | grep -q online ;then
+    pm2 start --name qsign116 "bash $HOME/QSignServer/qsign116/bin/unidbg-fetch-qsign --basePath=$HOME/QSignServer/txlib/${version}"
     echo
     echo -en ${yellow}签名服务器已经启动,是否打开日志 [Y/n]${background};read num
     case $num in
      Y|y)
-       pm2 logs qsign115
+       pm2 logs qsign116
        echo
        ;;
        esac
@@ -230,100 +201,87 @@ else
     echo -en ${yellow}签名服务器已经启动,是否打开日志 [Y/n]${background};read num
       case $num in
      Y|y)
-       pm2 logs qsign115
+       pm2 logs qsign116
        echo
        ;;
        esac  
 fi
 echo -en ${yellow}回车返回${background};read
-;;
-2|1.1.0)
-if [ ! -d $HOME/QSignServer/qsign110 ];then
-echo -e ${yellow}您未安装过该版本的签名服务器${background}
-exit
-fi
-if ! pm2 show qsign110 | grep -q online ;then
-    pm2 start --name qsign110 "bash $HOME/QSignServer/qsign110/bin/unidbg-fetch-qsign --host=0.0.0.0 --port=23500 --count=1 --library=$HOME/QSignServer/txlib/${version} --android_id=GGw0JRIMXn5a"
-    
-    echo
-    echo -en ${yellow}签名服务器已经启动,是否打开日志 [Y/n]${background};read num
-    case $num in
-     Y|y)
-       pm2 logs qsign110
-       echo
-       ;;
-       esac
-else
-    echo -en ${yellow}签名服务器已经启动,是否打开日志 [Y/n]${background};read num
-      case $num in
-     Y|y)
-       pm2 logs qsign110
-       echo
-       ;;
-       esac  
-fi
-echo -en ${yellow}回车返回${background};read
-;;
-esac
 }
 
 function stop_QSignServer(){
-echo -e ${white}"====="${green}白狐-QSignServer${white}"====="${background}
-echo -e ${green}请选择您要停止的签名服务器版本${background}
-echo -e  ${green}1.  ${cyan}1.1.5${background}
-echo -e  ${green}2.  ${cyan}1.1.0${background}
-echo "========================="
-echo -en ${green}请输入您的选项: ${background};read number
-case ${number} in
-1|1.1.5)
-pm2 stop qsign115
-pm2 delete qsign115
-;;
-2|1.1.0)
-pm2 stop qsign110
-pm2 delete qsign110
-;;
-*)
-echo
-echo -e ${red}输入错误${background}
-exit
-;;
-esac
+pm2 stop qsign116
+pm2 delete qsign116
+echo -e ${yellowyellow}停止完成 回车返回${background}
 }
 
 function restart_QSignServer(){
-echo -e ${white}"====="${green}白狐-QSignServer${white}"====="${background}
-echo -e ${green}请选择您要重启的签名服务器版本${background}
-echo -e  ${green}1.  ${cyan}1.1.5${background}
-echo -e  ${green}2.  ${cyan}1.1.0${background}
-echo "========================="
-echo -en ${green}请输入您的选项: ${background};read number
-case ${number} in
-1|1.1.5)
-pm2 restart qsign115
-;;
-2|1.1.0)
-pm2 restart qsign110
-;;
-*)
-echo
-echo -e ${red}输入错误${background}
-exit
-;;
-esac
+pm2 restart qsign116
+echo -e ${yellow}重启完成 回车返回${background}
+}
+
+function update_QSignServer(){
+rm -rf $HOME/QSignServer/txlib
+rm -rf $HOME/QSignServer/qsign*
+mkdir -p $HOME/QSignServer/txlib
+git clone --depth=1 -b 1.1.6 https://gitee.com/baihu433/unidbg-fetch-qsign
+rm -rf $HOME/QSignServer/txlib
+mv -f unidbg-fetch-qsign/txlib $HOME/QSignServer/txlib
+rm -rf unidbg-fetch-qsign
+until axel -n 32 -o qsign.zip -c ${QSIGN_URL}
+  do
+    echo -e ${red}下载失败 ${green}正在重试${background}
+  done
+echo -e ${yellow}正在解压qsign文件压缩包${background}
+    unzip -q qsign.zip -d qsign
+    mv $HOME/qsign/$(ls qsign) $HOME/QSignServer/qsign${QSIGN_VERSION}
+    rm -rf qsign.zip
+    rm -rf qsign
+    rm -rf $HOME/QSignServer/qsign${QSIGN_VERSION}/txlib 2&> /dev/null
+echo -en ${yellow}更新完成 回车返回${background};read
 }
 
 function uninstall_QSignServer(){
 cd $HOME
 echo -e ${yellow}正在停止服务器运行${background}
-pm2 stop qsign115
-pm2 delete qsign115
-pm2 stop qsign110
-pm2 delete qsign110
+pm2 stop qsign116
+pm2 delete qsign116
 rm -rf $HOME/QSignServer 2&> /dev/null
 rm -rf $HOME/QSignServer 2&> /dev/null
 }
 
+function key_QSignServer(){
+echo -e ${green}请输入更改后的key: ${background};read key_
+for file in $(ls $HOME/QSignServer/txlib)
+do
+file="$HOME/QSignServer/txlib/${file}/config.json"
+key="$(grep -E key ${file} | awk '{print $2}' | sed "s/\"//g" | sed "s/,//g" )"
+sed -i "s/${key}/${key_}/g" ${file}
+done
+echo -e ${yellow}更改完成 回车返回${background}
+}
+
+function port_QSignServer(){
+echo -e ${green}请输入更改后的端口号: ${background};read port_
+for file in $(ls $HOME/QSignServer/txlib)
+do
+file="$HOME/QSignServer/txlib/${file}/config.json"
+port="$(grep -E port ${file} | awk '{print $2}' | sed "s/\"//g" | sed "s/://g" )"
+sed -i "s/${port}/${port_}/g" ${file}
+done
+echo -e ${yellow}更改完成 回车返回${background}
+}
+
+if [ -d $HOME/QSignServer/qsign115 ];then
+qsign_version="1.1.5 ${red}[请更新]"
+elif [ -d $HOME/QSignServer/qsign116 ];then
+qsign_version=1.1.6
+file="$HOME/QSignServer/txlib/8.9.68/config.json"
+port="$(grep -E port ${file} | awk '{print $2}' | sed "s/\"//g" | sed "s/://g" )"
+key="$(grep -E key ${file} | awk '{print $2}' | sed "s/\"//g" | sed "s/,//g" )"
+host="$(grep -E host ${file} | awk '{print $2}' | sed "s/\"//g" | sed "s/,//g" )"
+API_LINK="${host}":"${port}"/sign?key="${key}"
+fi
 echo -e ${white}"====="${green}白狐-QSignServer${white}"====="${background}
 echo -e  ${green}1.  ${cyan}安装签名服务器${background}
 echo -e  ${green}2.  ${cyan}启动签名服务器${background}
@@ -331,14 +289,13 @@ echo -e  ${green}3.  ${cyan}关闭签名服务器${background}
 echo -e  ${green}4.  ${cyan}重启签名服务器${background}
 echo -e  ${green}5.  ${cyan}更新签名服务器${background}
 echo -e  ${green}6.  ${cyan}卸载签名服务器${background}
-#echo -e  ${green}7.  ${cyan}修改签名服务器key值${background}
-#echo -e  ${green}8.  ${cyan}修改签名服务器端口${background}
-echo -e  ${green}7.  ${cyan}清理签名服务器日志${background}
+echo -e  ${green}7.  ${cyan}修改签名服务器key值${background}
+echo -e  ${green}8.  ${cyan}修改签名服务器端口${background}
+echo -e  ${green}9.  ${cyan}清理签名服务器日志${background}
 echo -e  ${green}0.  ${cyan}退出${background}
 echo "========================="
-#echo -e ${green}您的api链接: "127.0.0.1":"${port}"/sign?key="${key}"${background}
-echo -e ${green}您的1.1.5版本的api链接: "127.0.0.1:8080/sign?key=114514"${background}
-echo -e ${green}您的1.1.0版本的api链接: "127.0.0.1:23500"${background}
+echo -e ${green}您的api链接: ${API_LINK}${background}
+echo -e ${green}当前签名服务器版本:${cyan}${qsign_version}${background}
 echo -e ${green}QQ群:${cyan}狐狸窝:705226976${background}
 echo "========================="
 echo
@@ -360,11 +317,22 @@ stop_QSignServer
 echo
 restart_QSignServer
 ;;
+5)
+echo
+update_QSignServer
+;;
 6)
 echo
 uninstall_QSignServer
 ;;
 7)
+echo
+key_QSignServer
+;;
+8)
+port_QSignServer
+;;
+9)
 echo
 pm2 flush qsign
 ;;
