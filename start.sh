4ef185a564e63deb9104149338999751b751ec19
baihu433 <1522099983@qq.com>
2023-07-28 12:18:14 +0000

    up

--- start.sh
+++ start.sh
@@ -11,6 +11,11 @@ echo -e "\033[32m请使用这条命令启动\033[0m"
 echo -e "\033[33mproot-distro login ubuntu\033[0m"
 exit 0
 fi
+echo -e '\033[36m请允许存储与后台权\033[0m'
+echo y | termux-setup-storage
+sleep 2s
+termux-wake-lock
+sleep 2s
 sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.bfsu.edu.cn/termux/termux-packages-24 stable main@' $PREFIX/etc/apt/sources.list
 yes Y | pkg update -y | pkg upgrade -y
 sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.bfsu.edu.cn/termux/termux-packages-24 stable main@' $PREFIX/etc/apt/sources.list
@@ -18,11 +23,7 @@ yes Y | pkg update -y | pkg upgrade -y
 sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.bfsu.edu.cn/termux/termux-packages-24 stable main@' $PREFIX/etc/apt/sources.list
 yes Y | pkg update -y && pkg upgrade -y
 apt install git wget proot-distro -y
-echo -e '\033[36m请允许存储与后台权\033[0m'
-sleep 1s
-echo y | termux-setup-storage
-sleep 3s
-termux-wake-lock
+apt install git wget proot-distro -y
 echo "proot-distro login ubuntu" > $PREFIX/bin/U
 chmod +x $PREFIX/bin/U
 echo >> $HOME/.bashrc
@@ -48,7 +49,10 @@ case $(uname -m) in
     exit
     ;;
 esac
-wget -O ubuntu-${ubuntu}-pd-v3.10.0.tar.xz https://ghproxy.com/https://github.com/termux/proot-distro/releases/download/v3.10.0/ubuntu-${ubuntu}-pd-v3.10.0.tar.xz
+until wget -O ubuntu-${ubuntu}-pd-v3.10.0.tar.xz https://ghproxy.com/https://github.com/termux/proot-distro/releases/download/v3.10.0/ubuntu-${ubuntu}-pd-v3.10.0.tar.xz
+do
+echo -e "\033[31m下载失败 重试中\033[0m"
+done
 mv -f ubuntu-${ubuntu}-pd-v3.10.0.tar.xz ../usr/var/lib/proot-distro/dlcache
 proot-distro install ubuntu
 wget -O YZ.sh https://gitee.com/baihu433/Ubuntu-Yunzai/raw/master/YZ.sh
@@ -57,6 +61,7 @@ mkdir ../usr/var/lib/proot-distro/installed-rootfs/ubuntu/root/.fox@bot
 echo > ../usr/var/lib/proot-distro/installed-rootfs/ubuntu/usr/bin/locale-check
 chmod +x ../usr/var/lib/proot-distro/installed-rootfs/ubuntu/usr/bin/locale-check
 echo "bash YZ.sh" >> ../usr/var/lib/proot-distro/installed-rootfs/ubuntu/root/.bashrc
+echo "1077:x:1077:" >> ../usr/var/lib/proot-distro/installed-rootfs/ubuntu/etc/group
 ln -s ../usr/var/lib/proot-distro/installed-rootfs/ubuntu/root/.fox@bot $HOME/fox@bot
 echo "proot-distro login ubuntu" > .bashrc
 proot-distro login ubuntu
