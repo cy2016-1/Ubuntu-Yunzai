b12913baae35cceed2defe524cc916f0a35bbcbe
baihu433 <1522099983@qq.com>
2023-08-02 04:40:25 +0000

    up:签名服务器部署脚本3.0

--- QSignServer3.0.sh
+++ QSignServer3.0.sh
@@ -25,11 +25,11 @@ QSIGN_VERSION="116"
 qsign_version=1.1.6
 case $(uname -m) in
 amd64|x86_64)
-JDK_URL="https://cdn.azul.com/zulu/bin/zulu8.70.0.23-ca-jdk8.0.372-linux_x64.tar.gz"
+JDK_URL="https://download.oracle.com/java/17/archive/jdk-17.0.8_linux-x64_bin.tar.gz"
 node=x64
 ;;
 arm64|aarch64)
-JDK_URL="https://cdn.azul.com/zulu-embedded/bin/zulu8.70.0.23-ca-jdk8.0.372-linux_aarch64.tar.gz"
+JDK_URL="https://download.oracle.com/java/17/archive/jdk-17.0.8_linux-aarch64_bin.tar.gz"
 node=arm64
 ;;
 esac
@@ -37,24 +37,6 @@ function main(){
 if [ -d $HOME/QSignServer/jdk ];then
 export PATH=$PATH:$HOME/QSignServer/jdk/bin
 export JAVA_HOME=$HOME/QSignServer/jdk
-fi
-if [ -d /usr/local/node/bin ];then
-    if [ ! -d $HOME/.local/share/pnpm ];then
-        mkdir -p $HOME/.local/share/pnpm
-    fi
-    export PATH=$PATH:$HOME/.local/share/pnpm
-    export PNPM_HOME=$HOME/.local/share/pnpm
-fi
-if [ -d $HOME/QSignServer/node/bin ];then
-export PATH=$PATH:$HOME/QSignServer/node/bin
-export PNPM_HOME=$HOME/QSignServer/node/bin
-fi
-if [ -d /usr/lib/node_modules/pnpm/bin ];then
-    if [ ! -d $HOME/.local/share/pnpm ];then
-        mkdir -p $HOME/.local/share/pnpm
-    fi
-    export PATH=$PATH:$HOME/.local/share/pnpm
-    export PNPM_HOME=$HOME/.local/share/pnpm
 fi
 function install_QSignServer(){
 if [ ! -e /etc/resolv.conf ]; then
@@ -68,25 +50,25 @@ if ! grep -q "114.114.114.114" /etc/resolv.conf && grep -q "8.8.8.8" /etc/resolv
     echo "nameserver 8.8.8.8" >> /etc/resolv.conf
     echo -e ${yellow}DNS已修改为 114.114.114.114 8.8.8.8${background}
 fi
-if [ ! $(command -v git) ] || [ ! $(command -v wget) ] || [ ! $(command -v gzip) ] || [ ! $(command -v unzip) ] || [ ! $(command -v xz) ] || [ ! $(command -v tar) ];then
+if [ ! $(command -v git) ] || [ ! $(command -v wget) ] || [ ! $(command -v gzip) ] || [ ! $(command -v unzip) ] || [ ! $(command -v xz) ] || [ ! $(command -v tar) ] || [ ! $(command -v tmux) ];then
     if [ $(command -v apt) ];then
         apt update -y
-        apt install -y git wget gzip unzip xz-utils tar
+        apt install -y git wget gzip unzip xz-utils tar tmux
     elif [ $(command -v yum) ];then
         yum update -y
-        yum install -y git wget gzip unzip xz tar
+        yum install -y git wget gzip unzip xz tar tmux
     elif [ $(command -v dnf) ];then
         dnf update -y
         dnf install -y git wget gzip unzip xz
     elif [ $(command -v pacman) ];then
-        pacman -Sy --noconfirm --needed git wget gzip unzip xz tar
+        pacman -Sy --noconfirm --needed git wget gzip unzip xz tar tmux
     elif [ $(command -v apk) ];then
         apk update
-        apk add git wget gzip unzip xz tar
+        apk add git wget gzip unzip xz tar tmux
     fi
 fi
 JAVA_VERSION=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}')
-if [[ ! "${JAVA_VERSION}" == "1.8"* ]]; then
+if [[ ! "$JAVA_VERSION" == "17."* ]]; then
     until wget -O jdk.tar.gz -c ${JDK_URL}
     do
       echo -e ${red}下载失败 ${green}正在重试${background}
@@ -94,6 +76,7 @@ if [[ ! "${JAVA_VERSION}" == "1.8"* ]]; then
     if [ ! -d $HOME/QSignServer ];then
         mkdir QSignServer
     fi
+    rm -rf jdk
     rm -rf QSignServer/jdk 2&> /dev/null
     echo -e ${yellow}正在解压JDK文件,请耐心等候${background}
     mkdir jdk
@@ -103,60 +86,6 @@ if [[ ! "${JAVA_VERSION}" == "1.8"* ]]; then
     rm -rf jdk
     PATH=$PATH:$HOME/QSignServer/jdk/bin
     export JAVA_HOME=$HOME/QSignServer/jdk
-fi
-NODEJS_URL16=https://cdn.npmmirror.com/binaries/node/latest-v16.x/node-v16.20.0-linux-${node}.tar.xz
-NODEJS_URL18=https://cdn.npmmirror.com/binaries/node/latest-v18.x/node-v18.17.0-linux-${node}.tar.xz
-Nodsjs_Version=$(node -v | cut -d '.' -f1)
-if ! [[ "$Nodsjs_Version" == "v16" || "$Nodsjs_Version" == "v18" ]]
-    then
-    if awk '{print $2}' /etc/issue | grep -q -E 22.*
-        then
-            curl -o node.tar.xz ${NODEJS_URL18}
-    elif awk '{print $2}' /etc/issue | grep -q -E 23.*
-        then
-            curl -o node.tar.xz ${NODEJS_URL18}
-    else
-            curl -o node.tar.xz ${NODEJS_URL16}
-    fi
-    if [ ! -d node ];then
-    mkdir node
-    fi
-    echo -e ${yellow}正在解压nodejs二进制文件压缩包${background}
-    rm -rf $HOME/QSignServer/node
-    tar -xJf node.tar.xz -C node
-    mv -f node/$(ls node) $HOME/QSignServer/node
-    rm -rf node.tar.xz
-    rm -rf node
-    export PATH=$PATH:$HOME/QSignServer/node/bin
-    export PNPM_HOME=$HOME/QSignServer/node/bin
-fi
-if ! [ -x "$(command -v pnpm)" ];then
-    echo -e ${yellow}正在使用npm安装pnpm${background}
-    a=0
-    npm config set registry https://registry.npmmirror.com
-    npm config set registry https://registry.npmmirror.com
-    npm install -g npm@latest
-    until npm install -g pnpm@latest
-    do
-      echo -e ${red}pnpm安装失败 ${green}正在重试${background}
-      a=$(($a+1))
-      if [ "${a}" == "3" ];then
-        echo -e ${red}错误次数过多 退出${background}
-        exit
-      fi
-    done
-    echo
-fi
-if ! [ -x "$(command -v pm2)" ];then
-    echo -e ${yellow}正在使用pnpm安装pm2${background}
-    pnpm config set registry https://registry.npmmirror.com
-    pnpm config set registry https://registry.npmmirror.com
-    until pnpm install -g pm2@latest
-    do
-      echo -e ${red}pm2安装失败 ${green}正在重试${background}
-      pnpm setup
-    done
-    echo
 fi
 if [ -d $HOME/QSignServer/qsign${QSIGN_VERSION} ];then
 echo -e ${yellow}您已安装过该版本的签名服务器了${background}
@@ -190,14 +119,33 @@ then
         port="$(grep -E port ${file} | awk '{print $2}' | sed "s/\"//g" | sed "s/://g" )"
         sed -i "s/\"port\": ${port}/\"port\": ${port_}/g" ${file}
     done
-    sed -i '/sign_api_addr/d' $HOME/.fox@bot/Yunzai-Bot/config/config/bot.yaml
-    sed -i '$a\sign_api_addr: https://127.0.0.1:6666/sign?key=114514'
-    echo -e ${cyan}已自动填入签名服务器链接${background}
-    echo -e ${yellow}正在启动签名服务器${background}
-    start_QSignServer
-    echo -e ${yellow}正在启动BOT${background}
+    file="$HOME/.fox@bot/Yunzai-Bot/config/config/bot.yaml"
+    sed -i '/sign_api_addr/d' ${file}
+    sed -i '$a\sign_api_addr: https://127.0.0.1:6666/sign?key=6666' ${file}
+    echo -e ${cyan}已自动填入API链接${background}
+    echo -e ${cyan}正在尝试启动BOT${background}
+    echo
+    echo -e ${white}"====="${green}白狐-QSignServer${white}"====="${background}
+    echo -e ${cyan}请选择您的启动方式${background}
+    echo -e  ${green}1. ${cyan}前台启动${background}
+    echo -e  ${green}2. ${cyan}后台启动${background}
+    echo "========================="
+    echo -en ${green}请输入您的选项: ${background};read num
+    case ${yn} in
+    1)
     cd $HOME/.fox@bot/Yunzai-Bot
     node app
+    ;;
+    2)
+    cd $HOME/.fox@bot/Yunzai-Bot
+    pnpm run start
+    ;;
+    *)
+    echo -e ${red}输入错误 默认前台启动${background}
+    cd $HOME/.fox@bot/Yunzai-Bot
+    node app
+    ;;
+    esac
 else
     echo -en ${yellow}安装完成 是否启动?[Y/n]${background};read yn
     case ${yn} in
@@ -257,31 +205,31 @@ if [ ! -d $HOME/QSignServer/qsign${QSIGN_VERSION} ];then
 echo -e ${yellow}您未安装过该版本的签名服务器${background}
 exit
 fi
-if ! pm2 show qsign${QSIGN_VERSION} | grep -q online ;then
-    pm2 start --name qsign${QSIGN_VERSION} "bash $HOME/QSignServer/qsign${QSIGN_VERSION}/bin/unidbg-fetch-qsign --basePath=$HOME/QSignServer/txlib/${version}"
-    echo
+cd $HOME/QSignServer
+if tmux ls | grep -q qsign${QSIGN_VERSION} ;then
     echo -en ${yellow}签名服务器已经启动,是否打开日志 [Y/n]${background};read num
-    case $num in
+      case $num in
      Y|y)
-       pm2 logs qsign${QSIGN_VERSION}
+       tail -f qsign.log
        echo
        ;;
        esac
 else
+    tmux new -s qsign${QSIGN_VERSION} "bash qsign${QSIGN_VERSION}/bin/unidbg-fetch-qsign --basePath=$HOME/QSignServer/txlib/${version}" | tee qsign.log
+    echo
     echo -en ${yellow}签名服务器已经启动,是否打开日志 [Y/n]${background};read num
-      case $num in
+    case $num in
      Y|y)
-       pm2 logs qsign${QSIGN_VERSION}
+       tail -f qsign.log
        echo
        ;;
-       esac  
+       esac
 fi
 echo -en ${yellow}回车返回${background};read
 }
 
 function stop_QSignServer(){
-pm2 stop qsign${QSIGN_VERSION}
-pm2 delete qsign${QSIGN_VERSION}
+tmux kill-session -t qsign${QSIGN_VERSION}
 echo -e ${yellow}停止完成 回车返回${background};read
 }
 
@@ -378,7 +326,7 @@ echo -en ${yellow}回车返回${background};read
 }
 
 if [ -d $HOME/QSignServer/qsign${QSIGN_VERSION} ];then
-    if pm2 show qsign${QSIGN_VERSION} | grep -q online;then
+    if tmux ls | grep qsign${QSIGN_VERSION};then
         condition="${cyan}[已启动]"
     else
         condition="${red}[未启动]"
